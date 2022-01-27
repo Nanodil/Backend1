df0424aedec5b8aef4bec5adbda32ed13154002a
nano <marianodilella86@gmail.com>
2022-01-27 18:38:49 -0300

    Primer commit

---
+++ DB/base.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+mongoose.connect(
+    process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+}, (err) => {
+    if (err) {
+       console.log('No Anda el server ',err)
+    } else{
+        console.log('Atlas listo')
+    }
+})
